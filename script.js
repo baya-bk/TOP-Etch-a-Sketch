2f9bd35d5769ec37d3039d188f047660f625a6af
Behailu <behailubedada1994@gmail.com>
2024-08-13 14:34:56 +0300

    Add cellCreator function to receive input from user and create cells

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16 * 16; i++) {
-  const square = document.createElement("div");
-  square.className = "square";
-  container.appendChild(square);
+let cellVal = prompt("Enter amount cells you want(must be less than 100): ");
+
+function cellCreator(cellVal) {
+  container.style.display = "grid";
+  container.style.gridTemplateColumns = `repeat(${cellVal}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${cellVal}, 1fr)`;
+  for (let i = 0; i < cellVal * cellVal; i++) {
+    const square = document.createElement("div");
+    square.className = "square";
+    square.style.border = "0.5px solid rgb(200, 200, 200)";
+    container.appendChild(square);
+  }
 }
+cellCreator(cellVal);
